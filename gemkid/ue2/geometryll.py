442f3aa3c3f42758761072625d7c4fa4c3390cc3
Aled Cuda <aled@ucsb.edu>
2025-12-03 16:04:06 -0800

    Ok apparently no defaults

--- gemkid/ue2/geometryll.py
+++ gemkid/ue2/geometryll.py
@@ -4,7 +4,7 @@ import gdstk
 from dataclasses import dataclass
 from typing import Optional
 
-from ..ue1.layers import HF_LL
+from ..ue1.layers import HF_LL, ATA_NB, HF_CONTACT
 from ..layers import DrawingLayer
 
 from . import geometry
@@ -14,56 +14,56 @@ from . import array
 class ViaWire(geometry.ViaWire):
     # bridge_width: float = 4
     bridge_layer: tuple[int, int] | DrawingLayer = HF_LL
-    # bridge_land: bool = True
-    # landing_width: float = 4
-    # landing_length: float = 18
-    # landing_layer: tuple[int, int] | DrawingLayer = ATA_NB
-    # via_width: float = 3
-    # via_length: float = 15
-    # via_layer: tuple[int, int] | DrawingLayer = HF_CONTACT
-    # liftoff_width: float = 5
-    # asi_width: float = 6
+    bridge_land: bool = True
+    landing_width: float = 4
+    landing_length: float = 18
+    landing_layer: tuple[int, int] | DrawingLayer = ATA_NB
+    via_width: float = 3
+    via_length: float = 15
+    via_layer: tuple[int, int] | DrawingLayer = HF_CONTACT
+    liftoff_width: float = 5
+    asi_width: float = 6
 
 
 @dataclass(eq=True, frozen=True)
 class InductorConfig(geometry.InductorConfig):
-    # legs: int = 4
-    # leg_gap: float = 2.0
-    # leg_length: float = 40.0
-    # leg_width: float = 4.0
-    # leg_landing: float = 8
+    legs: int = 4
+    leg_gap: float = 2.0
+    leg_length: float = 40.0
+    leg_width: float = 4.0
+    leg_landing: float = 8
     leg_layer: DrawingLayer = HF_LL
-    # wiring_width: float = 4.0
-    # wiring_gap: float = 2.0
-    # wiring_layer: DrawingLayer = ATA_NB
-    # wiring_extra: float = 8.0
-    # wiring_extra_height: float = 10.0
-    # via_over: Optional[float] = None
-    # via_width: float = 5.0
-    # via_inset: float = 5
-    # via_gap: float = 3
-    # via_layer: DrawingLayer = HF_CONTACT
-    # litho_vias: Optional[int] = 2
+    wiring_width: float = 4.0
+    wiring_gap: float = 2.0
+    wiring_layer: DrawingLayer = ATA_NB
+    wiring_extra: float = 8.0
+    wiring_extra_height: float = 10.0
+    via_over: Optional[float] = None
+    via_width: float = 5.0
+    via_inset: float = 5
+    via_gap: float = 3
+    via_layer: DrawingLayer = HF_CONTACT
+    litho_vias: Optional[int] = 2
 
 @dataclass(eq=True, frozen=True)
 class BoxConfig(geometry.BoxConfig):
-    inductor: Optional[InductorConfig]
-    # capacitor: Optional[geometry.CapacitorConfig]
-    # feedline: UEFeedlineConfig = UEFeedlineConfig(feed_layer=ATA_NB, ground_layer=ATA_NB)
-    # coupler_width: float = 2.0
-    # coupler_gap: float = 4.0
-    # coupler_fill: bool = False
-    # coupler_layer: DrawingLayer = ATA_NB
-    # coupler_via: Optional[mecstyle.ViaWire] = (
-    #     None  # mecstyle.ViaWire(2.0, HF, False, 2.0, 2.0, ATA_NB, 2.0, 2.0, HF_CONTACT)
-    # )
-    # box_width: float = 2.0
-    # box_gap: float = 1.0
-    # box_layer: DrawingLayer = ATA_NB
-    # width: float = 444
-    # height: float = 222
-    # extended_coupler_pullback: bool = False
-    # double_coupler: bool = True
+  inductor: Optional[InductorConfig]
+  capacitor: Optional[geometry.CapacitorConfig]
+  feedline: geometry.UEFeedlineConfig = geometry.UEFeedlineConfig(feed_layer=ATA_NB, ground_layer=ATA_NB)
+  coupler_width: float = 2.0
+  coupler_gap: float = 4.0
+  coupler_fill: bool = False
+  coupler_layer: DrawingLayer = ATA_NB
+  coupler_via: Optional[geometry.mecstyle.ViaWire] = (
+      None  # mecstyle.ViaWire(2.0, HF, False, 2.0, 2.0, ATA_NB, 2.0, 2.0, HF_CONTACT)
+  )
+  box_width: float = 2.0
+  box_gap: float = 1.0
+  box_layer: DrawingLayer = ATA_NB
+  width: float = 444
+  height: float = 222
+  extended_coupler_pullback: bool = False
+  double_coupler: bool = True
 
 if __name__ == "__main__":
     import numpy as np
